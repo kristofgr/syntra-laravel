7a2d11eeedbbeb00973723390321edc14eb6fdf9
kristofgr <36733406+kristofgr@users.noreply.github.com>
2023-05-12 15:10:25 +0200

    wip

--- road-rage/app/Http/Controllers/EntryController.php
+++ road-rage/app/Http/Controllers/EntryController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class EntryController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('entries.index');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->entries()->create($validated);
+    }
 }
---
+++ road-rage/resources/views/entries/index.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Entries') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+                <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                    <form method="POST" action="{{ route('entries.store') }}">
+                        @csrf
+
+                        <x-input-label for="plate" :value="__('Plate')" />
+                            <x-text-input id="plate" class="block w-full" type="plate" name="plate" :value="old('plate')" required autofocus />
+                            <x-input-error :messages="$errors->get('plate')" class="mt-2" />
+                        </div>
+
+                        <textarea
+                            name="message"
+                            placeholder="{{ __('What\'s on your mind?') }}"
+                            class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        >{{ old('message') }}</textarea>
+                        <x-input-error :messages="$errors->get('message')" class="mt-2" />
+                        <x-primary-button class="mt-4">{{ __('Add') }}</x-primary-button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
