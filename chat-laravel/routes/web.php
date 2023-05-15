d9176db927c6f1433fbc9994234e3c0bfd61b676
kristofgr <36733406+kristofgr@users.noreply.github.com>
2023-05-15 15:01:41 +0200

    form + open ai config

--- chat-laravel/routes/web.php
+++ chat-laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $messages = collect(session('messages', []))->reject(fn ($message) => $message['role'] === 'system');
+
+    return view('welcome', ['messages' => $messages]);
+});
+
+Route::post('/', function (Request $request) {
+    $messages = $request->session()->get('messages', [
+        ['role' => 'system', 'content' => 'You are LaravelGPT - A ChatGPT clone. Answer as concisely as possible.']
+    ]);
+
+    $messages[] = ['role' => 'user', 'content' => $request->input('message')];
+
+    $response = OpenAI::chat()->create([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => $messages
+    ]);
+
+    $messages[] = ['role' => 'assistant', 'content' => $response->choices[0]->message->content];
+
+    $request->session()->put('messages', $messages);
+
+    return redirect('/');
 });
