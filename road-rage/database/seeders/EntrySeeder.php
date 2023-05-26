3d63c2b77648c2894ca4b9e0218fb669bc80153f
kristofgr <36733406+kristofgr@users.noreply.github.com>
2023-05-26 10:51:57 +0200

    Entry Seeder
    Dankjewel Niels

--- road-rage/database/seeders/EntrySeeder.php
+++ road-rage/database/seeders/EntrySeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class EntrySeeder extends Seeder
 {
@@ -12,6 +16,22 @@ class EntrySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        // Get all user IDs from the 'users' table
+        $userIds = DB::table('users')->pluck('id');
+
+        // Seeding entries table
+        foreach ($userIds as $userId) {
+            for ($i = 0; $i < 50; $i++) {
+                DB::table('entries')->insert([
+                    'user_id' => $faker->randomElement($userIds),
+                    'plate' => $faker->regexify('[A-Z]{2}[0-9]{2}[A-Z]{2}'),
+                    'message' => $faker->sentence,
+                    'upvotes' => $faker->numberBetween(0, 9999),
+                    "created_at" => $faker->dateTimeBetween('-1 year', 'now')
+                ]);
+            }
+        }
     }
 }
